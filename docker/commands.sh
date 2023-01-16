24da3c25980d8a1a2bb2d53e4e7f04becbac7de8
root <yeganeh.nima.d@gmail.com>
2023-01-16 17:59:09 +0330

    done

---
+++ docker/commands.sh
@@ -0,0 +1,12 @@
+docker image pull ubuntu
+ps aux | grep container
+
+docker container run -it alpine ash
+ip a
+
+sudo apt-get install cgroup-tools
+sudo yum install libcgroup libcgroup-tools
+
+lssubsys -M
+
+lscgroup
