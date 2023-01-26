7298fb452dbdcc1e4a726c4444b61e6b461699cf
root <yeganeh.nima.d@gmail.com>
2023-01-26 19:16:22 +0330

    done

--- ansible-ubuntu/install-ansible.sh
+++ ansible-ubuntu/install-ansible.sh
@@ -1,4 +1,8 @@
 apt update -y
+apt install net-tools -y
+apt install apt-utils -y
+apt install inetutils-ping -y
+apt install git -y
 apt install software-properties-common -y
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
