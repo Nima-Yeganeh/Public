39cc67f731a9915ba3c90c26f91b7c648551aa51
root <yeganeh.nima.d@gmail.com>
2023-05-04 14:05:50 +0330

    done

---
+++ ubuntu-desktop/git_credential_manager_install2.sh
@@ -0,0 +1,3 @@
+wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.785/gcm-linux_amd64.2.0.785.deb 
+sudo dpkg -i gcm-linux_amd64.2.0.785.deb 
+rm gcm-linux_amd64.2.0.785.deb
