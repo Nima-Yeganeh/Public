8e4a2eac05ac7de8cff90a2afb42906f34e80e22
root <yeganeh.nima.d@gmail.com>
2023-05-04 14:38:47 +0330

    done

--- ubuntu-desktop/git_config.sh
+++ ubuntu-desktop/git_config.sh
@@ -1,5 +1,6 @@
 git config --global user.name ''
 git config --global user.email ''
+git config --global credential.credentialStore secretservice
 git config --list
 git-credential-manager-core help
 git-credential-manager-core configure
