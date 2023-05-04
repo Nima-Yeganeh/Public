cc194b68c0342157ad36a1843dde1f03a303c3ce
root <yeganeh.nima.d@gmail.com>
2023-05-04 13:53:39 +0330

    done

--- ubuntu-desktop/git_credential_manager_install.sh
+++ ubuntu-desktop/git_credential_manager_install.sh
@@ -1,3 +1,4 @@
 sudo apt-get install libsecret-1-0 libsecret-1-dev -y
 curl -sSL https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v2.0.498/gcmcore-linux_amd64.2.0.498.54650.deb -o gcmcore-linux_amd64.2.0.498.54650.deb
 sudo dpkg -i gcmcore-linux_amd64.2.0.498.54650.deb
+rm gcmcore-linux_amd64.2.0.498.54650.deb
