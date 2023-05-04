04ea61d42f3a8dde4533a897f0a6c6771aa8e72a
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-04 09:28:31 -0400

    done

--- ubuntu-desktop/kubernetes_config2.sh
+++ ubuntu-desktop/kubernetes_config2.sh
@@ -1,5 +1,13 @@
 sudo hostnamectl set-hostname kubernetes-master
 sudo hostnamectl set-hostname kubernetes-worker
 sudo kubeadm init
+
+mkdir -p $HOME/.kube
+kubernetes-master:~$ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+kubernetes-master:~$ sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubernetes-master:~$ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+
 kubectl get pods --all-namespaces
 kubectl get nodes
