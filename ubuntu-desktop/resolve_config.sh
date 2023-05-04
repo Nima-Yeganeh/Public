0759c4d2c21bc9592dff74933a989c5fb31de20f
root <yeganeh.nima.d@gmail.com>
2023-05-04 14:52:32 +0330

    done

--- ubuntu-desktop/resolve_config.sh
+++ ubuntu-desktop/resolve_config.sh
@@ -1,4 +1,2 @@
-sudo echo '' > /etc/resolv.conf
-sudo echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-sudo echo 'nameserver 1.1.1.1' >> /etc/resolv.conf
+sudo cp -f resolv.conf /etc/resolv.conf
 cat /etc/resolv.conf
