a763c8babc6728df6cc6924bdedbe45bcc3aa76f
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-04 08:55:49 -0400

    done

--- ubuntu-desktop/kubernetes_install2.sh
+++ ubuntu-desktop/kubernetes_install2.sh
@@ -1,6 +1,6 @@
-sudo apt update
-sudo apt install docker.io
-sudo apt install apt-transport-https curl
+sudo apt update -y
+sudo apt install docker.io -y
+sudo apt install apt-transport-https curl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt install kubeadm kubelet kubectl kubernetes-cni
+sudo apt install kubeadm kubelet kubectl kubernetes-cni -y
