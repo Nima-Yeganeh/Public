2cf9e71b265a7cb4f7f7dc4fa26008a461b5b1d2
root <yeganeh.nima.d@gmail.com>
2023-05-04 12:44:35 +0330

    done

---
+++ ubuntu-desktop/node_install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+node -v
+sudo apt-get install npm -y
+npm -v
--- ubuntu-desktop/update.sh
+++ ubuntu-desktop/update.sh
@@ -1,3 +1,1 @@
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo do-release-upgrade -y
