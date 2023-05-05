f56408b58b8f9fc33333c5a9c0fe838f0e701660
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-05 09:43:51 -0400

    done

--- ubuntu-desktop/gitlab_config.sh
+++ ubuntu-desktop/gitlab_config.sh
@@ -1,10 +1,14 @@
 sudo gitlab-rake gitlab:env:info
+sleep 5
 sudo ufw allow https
 sudo ufw allow http
 sudo ufw allow ssh
 sudo ufw reload
+sleep 5
 cat /etc/gitlab/gitlab.rb
+sleep 5
 sudo gitlab-ctl reconfigure
+sleep 5
 sudo gitlab-ctl start
 sudo gitlab-ctl status
 sudo gitlab-ctl start
