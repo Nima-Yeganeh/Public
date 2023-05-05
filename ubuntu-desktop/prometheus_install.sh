02e9c16cbbcf85f8c1585e776b794d424b7ea17c
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-05 08:02:49 -0400

    done

--- ubuntu-desktop/prometheus_install.sh
+++ ubuntu-desktop/prometheus_install.sh
@@ -3,3 +3,11 @@ tar xvfz prometheus-2.30.2.linux-amd64.tar.gz
 sudo mv prometheus-2.30.2.linux-amd64 /opt/prometheus
 sudo useradd --no-create-home --shell /bin/false prometheus
 sudo chown -R prometheus:prometheus /opt/prometheus
+sudo cp -f prometheus.service /etc/systemd/system/prometheus.service
+cat /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+echo 'http://localhost:9090'
+echo 'done!'
+
