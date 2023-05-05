b82d16d341e678be4547136acd9afeca997236c8
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-05 09:29:13 -0400

    done

--- ubuntu-desktop/gitlab_install.sh
+++ ubuntu-desktop/gitlab_install.sh
@@ -1,5 +1,11 @@
 sudo apt-get update -y
 sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-sudo apt-get install gitlab-ce
-sudo gitlab-ctl reconfigure
+sudo apt install -y tzdata curl ca-certificates openssh-server
+gpg_key_url="https://packages.gitlab.com/gitlab/gitlab-ce/gpgkey"
+curl -fsSL $gpg_key_url| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/gitlab.gpg
+sudo tee /etc/apt/sources.list.d/gitlab_gitlab-ce.list<<EOF
+deb https://packages.gitlab.com/gitlab/gitlab-ce/ubuntu/ focal main
+deb-src https://packages.gitlab.com/gitlab/gitlab-ce/ubuntu/ focal main
+EOF
+sudo apt update -y
+sudo apt-get install gitlab-ce -y
