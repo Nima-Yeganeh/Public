76d233e6376340ea7a120d6582ff95408d5fd0c7
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-05 12:14:20 -0400

    done

--- ubuntu-desktop/gitlab_runner_config.sh
+++ ubuntu-desktop/gitlab_runner_config.sh
@@ -1,2 +1,7 @@
 sudo gitlab-runner register
 sudo service gitlab-runner start
+sudo gitlab-runner restart
+sudo gitlab-runner status
+sudo gitlab-runner verify
+sudo gitlab-runner status
+sudo gitlab-runner start
