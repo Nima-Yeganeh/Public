932cf926e4d41ecf25f89898b2fddb84e65be19f
root <yeganeh.nima.d@gmail.com>
2023-05-06 14:55:12 +0330

    done

--- ubuntu-desktop/jenkins_install.sh
+++ ubuntu-desktop/jenkins_install.sh
@@ -1,9 +1,16 @@
 sudo apt update -y
 sudo apt install openjdk-17-jdk wget -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
 sudo apt update -y
 sudo apt install jenkins -y
+
 sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
 echo 'http://your_server_ip_or_domain:8080'
+
+sudo systemctl status jenkins
 
