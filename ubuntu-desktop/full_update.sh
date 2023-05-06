c2f45495bdaabdabf41e42ff5ef965a75b1a6923
root <yeganeh.nima.d@gmail.com>
2023-05-06 17:17:24 +0330

    done

--- ubuntu-desktop/full_update.sh
+++ ubuntu-desktop/full_update.sh
@@ -1,21 +1,2 @@
-sudo apt-get update -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install build-essential -y
-gcc --version
-g++ --version
-sudo apt-get install nodejs -y
-node -v
-sudo apt-get install npm -y
-npm -v
-sudo apt-get install python3 -y
-python3 --version
-sudo apt-get update -y
-sudo apt-get install net-tools -y
-sudo apt-get install iotop iftop -y
-sudo apt-get install golang -y
-go version
-export GOPATH=$HOME/go
-export GOROOT=/usr/lib/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-source ~/.bashrc
+sudo apt-get update -y --no-install-recommends
+sudo apt-get upgrade -y --no-install-recommends
