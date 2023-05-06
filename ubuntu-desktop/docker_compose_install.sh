beddd26c36425d6038565f226eb029094038ae12
root <yeganeh.nima.d@gmail.com>
2023-05-06 15:54:01 +0330

    done

---
+++ ubuntu-desktop/docker_compose_install.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
--- ubuntu-desktop/docker_install.sh
+++ ubuntu-desktop/docker_install.sh
@@ -3,6 +3,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker run hello-world
