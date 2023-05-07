e247d27e84b588f0d11a49bfec3034a5a986004c
root <yeganeh.nima.d@gmail.com>
2023-05-07 14:22:39 +0330

    done

--- ubuntu-desktop/minikube_install.sh
+++ ubuntu-desktop/minikube_install.sh
@@ -1,7 +1,11 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo systemctl start docker
+sudo systemctl enable docker
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
+sudo apt install kubectl
 minikube start
-kubectl get pods --all-namespaces
+kubectl version
+docker version
+minikube version
---
+++ ubuntu-desktop/minikube_install2.sh
@@ -0,0 +1,7 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+minikube start
+kubectl get pods --all-namespaces
