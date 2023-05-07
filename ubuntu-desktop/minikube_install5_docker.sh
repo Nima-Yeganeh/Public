79436d6036f2ecfe74d4ad3a5c50110109ae968d
root <yeganeh.nima.d@gmail.com>
2023-05-07 16:02:15 +0330

    done

--- ubuntu-desktop/minikube_install5_docker.sh
+++ ubuntu-desktop/minikube_install5_docker.sh
@@ -23,4 +23,4 @@ kubectl create deployment hello-minikube --image=kicbase/echo-server:1.0
 kubectl expose deployment hello-minikube --type=NodePort --port=8080
 kubectl get services hello-minikube
 minikube service hello-minikube
-kubectl port-forward service/hello-minikube 7080:8080
+# kubectl port-forward service/hello-minikube 7080:8080
