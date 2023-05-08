e197d247f416c0e1cf880c53bb1ccdb705dec77b
root <yeganeh.nima.d@gmail.com>
2023-05-08 19:10:15 +0330

    done

---
+++ ubuntu-desktop/nodejs_example/test12.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+
+// execute an Ubuntu administration command
+exec("sudo apt-get update", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Error: ${stderr}`);
+    return;
+  }
+  console.log(`Output: ${stdout}`);
+});
