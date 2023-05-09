cb1be3b9f74113fd6e705adbb03edfbe9765c767
root <yeganeh.nima.d@gmail.com>
2023-05-09 08:58:55 +0330

    done

--- ubuntu-desktop/g++_example/test3/main.cpp
+++ ubuntu-desktop/g++_example/test3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> numbers = {5, 2, 8, 3, 1, 6, 4, 7};
+    std::cout << "Unsorted numbers:";
+    for (int n : numbers) {
+        std::cout << " " << n;
+    }
+    std::cout << std::endl;
+    std::sort(numbers.begin(), numbers.end());
+    std::cout << "Sorted numbers:";
+    for (int n : numbers) {
+        std::cout << " " << n;
+    }
+    std::cout << std::endl;
     return 0;
 }
