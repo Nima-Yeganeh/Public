5a2fce5aa7468c6422b2e2b2a09c1821069dda29
root <yeganeh.nima.d@gmail.com>
2023-05-12 22:08:52 +0330

    done

--- ubuntu-desktop/jenkins_install.sh
+++ ubuntu-desktop/jenkins_install.sh
@@ -1,30 +1,22 @@
 sudo apt update -y
-sudo apt install openjdk-17-jdk wget -y
-sudo apt install default-jdk -y
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt install openjdk-17-jdk wget -y
+sudo apt install default-jdk -y
+sudo apt-get install jenkins -y
 sudo apt update -y
-
-# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-# sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
-
-sudo apt update -y
-sudo apt install jenkins -y
-
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-
 echo 'http://your_server_ip_or_domain:8080'
-
 sudo systemctl status jenkins
-
 ufw allow 8088
 # sudo ufw enable
+cat /var/lib/jenkins/secrets/initialAdminPassword
