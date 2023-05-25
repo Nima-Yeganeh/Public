327d7437d2479e8c685dc52f09dd86697dcceb3c
root <yeganeh.nima.d@gmail.com>
2023-05-25 17:11:25 +0330

    done

---
+++ wordpress/docker-test5/set_host_config_rev2.sh
@@ -0,0 +1,3 @@
+bash commands_host1_ver3.sh
+bash commands_host2_ver3.sh
+bash commands_host3_ver3.sh
--- wordpress/docker-test5/up.sh
+++ wordpress/docker-test5/up.sh
@@ -2,5 +2,5 @@ echo '**** Started ****'
 bash reload.sh
 echo '**** Configuring ****'
 sleep 10
-bash set_host_config_rev1.sh
+bash set_host_config_rev2.sh
 echo '**** Done ****'
