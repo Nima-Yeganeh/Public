2c9419b47bbbef4496b3e0497f7ee2fcfb6c45cf
root <yeganeh.nima.d@gmail.com>
2023-05-25 16:11:45 +0330

    done

--- wordpress/docker-test5/down.sh
+++ wordpress/docker-test5/down.sh
@@ -1,8 +1,10 @@
 docker compose down
 docker volume rm docker-test5_db1_data
-docker volume rm docker-test5_db2_data
 docker volume rm docker-test5_redis1_data
-docker volume rm docker-test5_redis2_data
 docker volume rm docker-test5_wordpress1_content
+docker volume rm docker-test5_db2_data
+docker volume rm docker-test5_redis2_data
 docker volume rm docker-test5_wordpress2_content
-
+docker volume rm docker-test5_db3_data
+docker volume rm docker-test5_redis3_data
+docker volume rm docker-test5_wordpress3_content
--- wordpress/docker-test5/reload.sh
+++ wordpress/docker-test5/reload.sh
@@ -1,10 +1,13 @@
 docker compose down
 docker volume rm docker-test5_db1_data
-docker volume rm docker-test5_db2_data
 docker volume rm docker-test5_redis1_data
-docker volume rm docker-test5_redis2_data
 docker volume rm docker-test5_wordpress1_content
+docker volume rm docker-test5_db2_data
+docker volume rm docker-test5_redis2_data
 docker volume rm docker-test5_wordpress2_content
+docker volume rm docker-test5_db3_data
+docker volume rm docker-test5_redis3_data
+docker volume rm docker-test5_wordpress3_content
 docker compose up -d
 docker ps -a
 
