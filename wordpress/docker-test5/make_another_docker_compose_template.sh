fd604ada6e39dfdc10492ecd0ea4a10977e5357e
root <yeganeh.nima.d@gmail.com>
2023-05-25 18:50:04 +0330

    done

---
+++ wordpress/docker-test5/make_another_docker_compose_template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user to enter a template number
+echo -n "Enter template number: "
+read template_number
+
+# Print the template number
+echo "Template number: $template_number"
+
+cat docker_compose_template | sed 's/db1/db$template_number/g' | sed 's/redis1/redis$template_number/g' | sed 's/wordpress1/wordpress$template_number/g'
