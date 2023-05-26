74daada3c960564bdad8d67bf79aebb5f82d1563
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-26 17:59:29 +0000

    done

--- wordpress/docker-test5/make_another_command_host_by_num.sh
+++ wordpress/docker-test5/make_another_command_host_by_num.sh
@@ -14,7 +14,7 @@ while IFS=',' read -r col1 col2 col3; do
         echo "$col1"
         echo "$col2"
         echo "$col3"
-        cat $tempfile | sed "s/docker-test5-wordpress1/docker-test5-wordpress"$col1"/g" | sed "s/domain.local/"$col3"domain.local/g" >> "commands_host"$col1"_ver3.sh"
+        cat $tempfile | sed "s/docker-test5-wordpress1/docker-test5-wordpress"$col1"/g" | sed "s/http:\/\/domain.local/http:\/\/"$col3".domain.local/g" > "commands_host"$col1"_ver3.sh"
     fi
     (( line_number++ ))
 done < $filename
