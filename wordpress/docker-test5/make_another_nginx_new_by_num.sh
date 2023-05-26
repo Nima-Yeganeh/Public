b19792f30f46fb77e6d6f0c99620a2585008ea00
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-26 16:48:29 +0000

    done

--- wordpress/docker-test5/make_another_nginx_new_by_num.sh
+++ wordpress/docker-test5/make_another_nginx_new_by_num.sh
@@ -10,6 +10,7 @@ filename="zdatafile.csv"
 nginxfile="nginx_temp"
 newnginxfile="nginx_new"
 touch $newnginxfile
+rm -f $newnginxfile
 # Loop through the lines in the file within the specified range
 line_number=1
 while IFS=',' read -r col1 col2 col3; do
@@ -17,7 +18,8 @@ while IFS=',' read -r col1 col2 col3; do
         echo "$col1"
         echo "$col2"
         echo "$col3"
-        cat $nginxfile | sed "s/domain.local/"$col3".domain.local/g" >> $newnginxfile
+        cat $nginxfile | sed "s/domain.local/"$col3".domain.local/g" | sed "s/wordpress1/wordpress"$col1"/g" | sed "s/redis1/redis"$col1"/g" >> $newnginxfile
     fi
     (( line_number++ ))
 done < $filename
+
