5c150dd5a52891ca82e6338f202b8c9999132d4b
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-26 15:00:07 +0000

    done

--- wordpress/docker-test5/down.sh
+++ wordpress/docker-test5/down.sh
@@ -11,3 +11,20 @@ docker volume rm docker-test5_wordpress3_content
 docker volume rm docker-test5_db4_data
 docker volume rm docker-test5_redis4_data
 docker volume rm docker-test5_wordpress4_content
+
+docker volume rm docker-test5_db5_data
+docker volume rm docker-test5_redis5_data
+docker volume rm docker-test5_wordpress5_content
+
+docker volume rm docker-test5_db6_data
+docker volume rm docker-test5_redis6_data
+docker volume rm docker-test5_wordpress6_content
+
+docker volume rm docker-test5_db7_data
+docker volume rm docker-test5_redis7_data
+docker volume rm docker-test5_wordpress7_content
+
+docker volume rm docker-test5_db8_data
+docker volume rm docker-test5_redis8_data
+docker volume rm docker-test5_wordpress8_content
+
